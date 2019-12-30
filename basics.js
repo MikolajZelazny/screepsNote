1f98084cb97ee9e94d7725fd414088738728f769
MikolajZelazny <MikolajZelazny.it@gmail.com>
2019-12-30 21:09:33 +0100

    send creep for collecting

--- basics.js
+++ basics.js
@@ -1,4 +1,15 @@
 Game // klasa w której jest cała gra.
-Game.spawns // spawny
-Game.spawns['<nazwa spawnu>'] // wybranie spawnu
-Game.spawns['<nazwa spawnu>'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester1' ); // rekrutacja pracownika 
+Game.spawns // spawny.
+Game.spawns['<nazwa spawnu>'] // wybranie spawnu.
+Game.spawns['<nazwa spawnu>'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester1' ); // rekrutacja pracownika.
+
+harvest(target) // wymaga żródła przylegającego do creepa oraz: WORK,CARRY,MOVE
+
+// wysłanie creepa do zbierania.
+module.exports.loop = function () {
+    var creep = Game.creeps['Harvester1']; // sterowanie creepem po jego imieniu.
+    var sources = creep.room.find(FIND_SOURCES); // "find" znajduje wszystko w pokoju, "FIND_SOURCES" filtruje wyniki
+    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) { // czy źródło przylega do creepa
+        creep.moveTo(sources[0]); // jeśli po za zasięgiem przemieść się do źródła podanego w filtrze
+    }
+}
