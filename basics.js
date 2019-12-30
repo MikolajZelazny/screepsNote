1daee7d8f8b776c978bbb998527524ec0c5b0992
MikolajZelazny <MikolajZelazny.it@gmail.com>
2019-12-30 19:42:09 +0100

    first look

---
+++ basics.js
@@ -0,0 +1,4 @@
+Game // klasa w której jest cała gra.
+Game.spawns // spawny
+Game.spawns['<nazwa spawnu>'] // wybranie spawnu
+Game.spawns['<nazwa spawnu>'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester1' ); // rekrutacja pracownika 
