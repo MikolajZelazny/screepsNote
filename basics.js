e000df1cbf1746cbeec16265a9e84e375b262dca
MikolajZelazny <mikolajzelazny98@gmail.com>
2020-01-01 17:38:42 +0100

    organising harvesters behavior into separate moodules

--- basics.js
+++ basics.js
@@ -56,3 +56,39 @@ module.exports.loop = function () {
         }
     }
 }
+
+
+// Kolejna porawa - przeniesienie zachowania robotników do osobnego modułu. / Organising harvesters behavior into separate moodules
+
+var roleHarvester = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) { // FUNKCJA NAPISANA POWYŻEJ
+	    if(creep.store.getFreeCapacity() > 0) {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
+            }
+        }
+        else {
+            if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(Game.spawns['Spawn1']);
+            }
+        }
+	}
+};
+
+module.exports = roleHarvester; 
+
+
+// MAIN MODULE CODE REWRITE - pozostawienie jedynie pętli "for" i odwołanie się do nowego modułu.
+
+var roleHarvester = require('role.harvester'); // metoda wzywająca moduł "role.harvester"
+
+module.exports.loop = function () {
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        roleHarvester.run(creep);
+    }
+}
