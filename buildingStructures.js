9215cd9b69e7e758ae826b553ea8b2d9f20d8124
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-01-07 19:42:52 +0100

    carrying energy to other structures

--- buildingStructures.js
+++ buildingStructures.js
@@ -33,7 +33,7 @@ var roleBuilder = {
 	    }
 	    else { // jeżeli builind == false, czyli że creep nie ma surowców to idzie zbierać
 	        var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+            if(creep.harvzest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
 	    }
@@ -41,3 +41,36 @@ var roleBuilder = {
 };
 
 module.exports = roleBuilder;
+
+
+// Refine the logic in the module role.harvester.
+// Zmiana zachowania zbieraczy na to by teraz przynosili surowce również do nowych budynków: STRUCTURE_EXTENSION
+
+
+var roleHarvester = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+	    if(creep.store.getFreeCapacity() > 0) { // standardnowy program zbierania energii gdy wolne miejsce w eq
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        }
+        else {
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                    }
+            });
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+        }
+	}
+};
+
+module.exports = roleHarvester;
